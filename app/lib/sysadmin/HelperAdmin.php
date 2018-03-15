97f654415d3a1eca8abfe6e30a14e4d1f2f82f55
maylon <maylon.correa20@gmail.com>
2018-03-15 16:15:56 -0300

    novamente

--- app/lib/sysadmin/HelperAdmin.php
+++ app/lib/sysadmin/HelperAdmin.php
@@ -12,6 +12,7 @@ class HelperAdmin{
      */
     public static function getSession($name = null)
     {
+    	$tetete = 0;
         if($name == null) $name = "sysadmin_unique_session";
         return Session::getValueIf($name);
     }
